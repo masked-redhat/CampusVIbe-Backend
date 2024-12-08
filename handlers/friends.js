c0d45b794a80120f244f1e77338d6a1213e80731
Zed <skhull.coding@proton.me>
2024-12-08 15:25:32 +0530

    setup friends handler

--- handlers/friends.js
+++ handlers/friends.js
@@ -1,1 +1,111 @@
-import codes from "../utils/status_codes.js"
+import Friend from "../controllers/friends.js";
+import codes from "../utils/status_codes.js";
+
+const handleGetFriends = async (req) => {
+  const friend = new Friend(req.user.uid);
+
+  let friends = await friend.friendsList();
+
+  let statusCode = codes.get.OK;
+  let message = "got friends for you";
+
+  return { statusCode, message, friends };
+};
+
+const handleGetRequests = async (req) => {
+  const friend = new Friend(req.user.uid, req.query);
+
+  let requests = await friend.getRequests();
+
+  let statusCode = codes.get.OK;
+  let message = "got requests for you";
+
+  return { statusCode, message, requests };
+};
+
+const handleSendRequest = async (req) => {
+  const friend = new Friend(req.user.uid, req.body);
+  await friend.checkDatabase();
+
+  let requestSent = await friend.sendRequest();
+
+  let statusCode, message;
+
+  if (requestSent) {
+    statusCode = codes.post.CREATED;
+    message = "request sent to the user";
+  } else {
+    statusCode = codes.clientError.BAD_REQUEST;
+    message = "try again later";
+  }
+
+  return { statusCode, message };
+};
+
+const handleAcceptRequest = async (req) => {
+  const friend = new Friend(req.user.uid, req.body);
+  await friend.checkDatabase();
+
+  let requestAccepted = await friend.acceptRequest();
+
+  let statusCode, message;
+
+  if (requestAccepted) {
+    statusCode = codes.put.OK;
+    message = "request accepted";
+  } else {
+    statusCode = codes.clientError.BAD_REQUEST;
+    message = "try again";
+  }
+
+  return { statusCode, message };
+};
+
+const handleCancelRequest = async (req) => {
+  const friend = new Friend(req.user.uid, req.query);
+  await friend.checkDatabase();
+
+  let requestCancel = await friend.cancelRequest();
+
+  let statusCode, message;
+
+  if (requestCancel) {
+    statusCode = codes.delete.NO_CONTENT;
+    message = "request cancelled";
+  } else {
+    statusCode = codes.clientError.BAD_REQUEST;
+    message = "try again";
+  }
+
+  return { statusCode, message };
+};
+
+const handleUnfriend = async (req) => {
+  const friend = new Friend(req.user.uid, req.query);
+  await friend.checkDatabase();
+
+  let removeFriend = await friend.removeFriend();
+
+  let statusCode, message;
+
+  if (removeFriend) {
+    statusCode = codes.delete.NO_CONTENT;
+    message = "request cancelled";
+  } else {
+    statusCode = codes.clientError.BAD_REQUEST;
+    message = "try again";
+  }
+
+  return { statusCode, message };
+};
+
+const handlers = {
+  handleGetFriends,
+  handleGetRequests,
+  handleAcceptRequest,
+  handleCancelRequest,
+  handleSendRequest,
+  handleUnfriend,
+};
+
+export default handlers;
--- routes/friends.js
+++ routes/friends.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import authenticate from "../middleware/authentication.js";
-import Friend from "../controllers/friends.js";
 import codes from "../utils/status_codes.js";
 import upload from "../middleware/parser.js";
+import handlers from "../handlers/friends.js";
 
 const router = Router();
 
@@ -12,114 +12,54 @@ router.use(upload.none());
 
 // Get friends
 router.get("/", async (req, res) => {
-  const friend = new Friend(req.user.uid);
-
-  let friends = await friend.friendsList();
-
-  let statusCode = codes.get.OK;
-  let message = "got friends for you";
+  const { statusCode, message, friends } = await handlers.handleGetFriends(req);
 
   res.status(statusCode).json({ message, friends });
 });
 
 // Get requests
 router.get("/request", async (req, res) => {
-  const friend = new Friend(req.user.uid, req.query);
-
-  let requests = await friend.getRequests();
-
-  let statusCode = codes.get.OK;
-  let message = "got requests for you";
+  const { statusCode, message, requests } = await handlers.handleGetRequests(
+    req
+  );
 
   res.status(statusCode).json({ message, requests });
 });
 
 // Send friend request
 router.post("/request", async (req, res) => {
-  const friend = new Friend(req.user.uid, req.body);
-  await friend.checkDatabase();
-
-  let requestSent = await friend.sendRequest();
-
-  let statusCode, message;
-
-  if (requestSent) {
-    statusCode = codes.post.CREATED;
-    message = "request sent to the user";
-  } else {
-    statusCode = codes.clientError.BAD_REQUEST;
-    message = "try again later";
-  }
+  const { statusCode, message } = await handlers.handleSendRequest(req);
 
   res.status(statusCode).json({ message });
 });
 
 // Accept friend request
 router.put("/request", async (req, res) => {
-  const friend = new Friend(req.user.uid, req.body);
-  await friend.checkDatabase();
-
-  let requestAccepted = await friend.acceptRequest();
-
-  let statusCode, message;
-
-  if (requestAccepted) {
-    statusCode = codes.put.OK;
-    message = "request accepted";
-  } else {
-    statusCode = codes.clientError.BAD_REQUEST;
-    message = "try again";
-  }
+  const { statusCode, message } = await handlers.handleAcceptRequest(req);
 
   res.status(statusCode).json({ message });
 });
 
 // Cancel Request
 router.delete("/request", async (req, res) => {
-  const friend = new Friend(req.user.uid, req.query);
-  await friend.checkDatabase();
-
-  let requestCancel = await friend.cancelRequest();
-
-  let statusCode, message;
-
-  if (requestCancel) {
-    statusCode = codes.delete.NO_CONTENT;
-    message = "request cancelled";
-  } else {
-    statusCode = codes.clientError.BAD_REQUEST;
-    message = "try again";
-  }
+  const { statusCode, message } = await handlers.handleCancelRequest(req);
 
   res.status(statusCode).json({ message });
 });
 
 // Unfriending
 router.delete("/", async (req, res) => {
-  const friend = new Friend(req.user.uid, req.query);
-  await friend.checkDatabase();
-
-  let removeFriend = await friend.removeFriend();
-
-  let statusCode, message;
-
-  if (removeFriend) {
-    statusCode = codes.delete.NO_CONTENT;
-    message = "request cancelled";
-  } else {
-    statusCode = codes.clientError.BAD_REQUEST;
-    message = "try again";
-  }
+  const { statusCode, message } = await handlers.handleUnfriend(req);
 
   res.status(statusCode).json({ message });
 });
 
-router.all("/", (req, res) => {
-  res.sendStatus(405);
+router.all("/", (_, res) => {
+  res.sendStatus(codes.clientError.METHOD_NOT_ALLOWED);
 });
 
-router.all("/request", (req, res) => {
-  res.sendStatus(405);
+router.all("/request", (_, res) => {
+  res.sendStatus(codes.clientError.METHOD_NOT_ALLOWED);
 });
 
 export const FriendRouter = router;

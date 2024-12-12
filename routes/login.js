eb2a21ebeb12e9fa1449f7043c883dbb7b1cdf2e
Zed <skhull.coding@proton.me>
2024-12-12 17:21:09 +0530

    routes

---
+++ routes/login.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import authentication from "../middlewares/authentication.js";
+
+const router = Router();
+
+router.post("/", (req, res) => {
+        
+
+
+
+});
+
+router.post("/new", (req, res) => {
+    
+})
+
+export const LoginRouter = router;

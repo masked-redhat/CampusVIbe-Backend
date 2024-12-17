0bba356bfe0884096fffb27a8ca46a396e2c4c1a
Zed <skhull.coding@proton.me>
2024-12-17 22:34:56 +0530

    check if an error contains an error code

--- utils/checks.js
+++ utils/checks.js
@@ -10,10 +10,22 @@ const isNotNuldefined = (entity) => {
   return entity != null && entity != undefined;
 };
 
+const isErrorCode = (err, code = 1062) => {
+  let errno;
+  try {
+    errno = err.errno ?? err.original.errno ?? err.parent.errno;
+  } catch (err) {
+    console.log(err);
+    errno = null;
+  }
+  return errno === code;
+};
+
 const checks = {
   isNull,
   isTrue,
   isNotNuldefined,
+  isErrorCode,
 };
 
 export default checks;

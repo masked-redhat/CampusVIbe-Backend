896b9b551c328e3d2f03d12511162bd2eecc8fe7
Zed <skhull.coding@proton.me>
2024-12-19 21:49:05 +0530

    posts handler

--- controllers/post.js
+++ controllers/post.js
@@ -21,6 +21,8 @@ const POST_DELETION_MESSAGE = "Post deleted successfully";
 const BAD_REQUEST_CODE = codes.clientError.BAD_REQUEST;
 const TITLE_NOT_GIVEN_MESSAGE = "No title given";
 const POST_ID_NOT_GIVEN_MESSAGE = "No post Id given";
+const POST_ID_WRONG_MESSAGE = "Post Id given is wrong";
+const BAD_REQUEST_MESSAGE = "Bad request";
 
 const NOT_FOUND_CODE = codes.clientError.NOT_FOUND;
 const POST_NOT_FOUND_MESSAGE = "No post with the credentials given";
@@ -32,6 +34,7 @@ class PostHandler {
   #postId;
   #userId;
   #limit = 10;
+  #postsOrder = [["createdAt", "DESC"]];
 
   constructor(user, reqBody = null, reqFile = null) {
     this.username = user?.username ?? null;
@@ -40,6 +43,8 @@ class PostHandler {
     this.image = reqFile?.filename ?? null;
     this.offsetAmt = reqBody?.offset ?? 0;
     this.#postId = reqBody?.id ?? null;
+
+    this.offsetAmt = parseInt(this.offsetAmt);
   }
 
   setup = async (res) => {
@@ -59,8 +64,9 @@ class PostHandler {
     try {
       posts = await Post.findAll({
         where: { userId: this.#userId },
-        offset: this.offsetAmt,
         limit: this.#limit,
+        offset: this.offsetAmt,
+        order: this.#postsOrder,
       });
     } catch (err) {
       console.log(err);
@@ -103,7 +109,7 @@ class PostHandler {
 
   // updating as per PATCH or PUT requests
   updatePost = async (res, type = REQ_TYPE.PUT) => {
-    if (checks.isNull(this.title))
+    if (type === REQ_TYPE.PUT && checks.isNull(this.title))
       return responses.serve(res, BAD_REQUEST_CODE, TITLE_NOT_GIVEN_MESSAGE);
     if (checks.isNull(this.#postId))
       return responses.serve(res, NOT_FOUND_CODE, POST_ID_NOT_GIVEN_MESSAGE);
@@ -114,36 +120,54 @@ class PostHandler {
       image: this.image,
     };
 
-    if (type === REQ_TYPE.PATCH)
+    if (type === REQ_TYPE.PATCH) {
       for (const uData in updatingData)
         if (checks.isNull(updatingData[uData])) delete updatingData[uData];
+    } else if (type !== REQ_TYPE.PUT)
+      return responses.serve(res, BAD_REQUEST_CODE, BAD_REQUEST_MESSAGE);
 
     let post;
-  try {
-    post = await Post.update(toBeUpdated, {
-      where: {
-        id: this.#postId,
-        userId: this.#userId,
-      },
-    });
-  } catch (err) {
-    console.log(err);
+    try {
+      post = await Post.update(updatingData, {
+        where: {
+          id: this.#postId,
+          userId: this.#userId,
+        },
+      });
+    } catch (err) {
+      console.log(err);
 
-    return responses.serve(res, SERVER_ERROR_CODE, SERVER_ERROR_MESSAGE);
-  }
+      return responses.serve(res, SERVER_ERROR_CODE, SERVER_ERROR_MESSAGE);
+    }
 
-  // if update occured, send the OK and if not NOT_FOUND
-  if (post[0] === 0) {
-    statusCode = codes.patch.NOT_FOUND;
-    message = "No post with the credentials given found";
-    return responses.serve(res, NOT_FOUND_CODE, message);
-  } else {
-    statusCode = codes.patch.OK;
-    message = "Post updated";
-    return responses.serve(res, SERVER_ERROR_CODE, SERVER_ERROR_MESSAGE);
-  }
+    if (checks.isTrue(post[0], 0))
+      return responses.serve(res, NOT_FOUND_CODE, POST_NOT_FOUND_MESSAGE);
 
-  responses.serve(res, statusCode, message);
+    return responses.serve(res, POST_UPDATION_CODE, POST_UPDATION_MESSAGE);
+  };
+
+  deletePost = async (res) => {
+    if (checks.isNull(this.#postId))
+      return responses.serve(res, BAD_REQUEST_CODE, POST_ID_NOT_GIVEN_MESSAGE);
+
+    let deleted;
+    try {
+      deleted = await Post.destroy({
+        where: {
+          id: this.#postId,
+          userId: this.#userId,
+        },
+      });
+    } catch (err) {
+      console.log(err);
+
+      return responses.serve(res, SERVER_ERROR_CODE, SERVER_ERROR_MESSAGE);
+    }
+
+    if (checks.isTrue(deleted[0], 0))
+      return responses.serve(res, BAD_REQUEST_CODE, POST_ID_WRONG_MESSAGE);
+
+    return responses.serve(res, POST_DELETION_CODE, POST_DELETION_MESSAGE);
   };
 }
 
--- routes/posts.js
+++ routes/posts.js
@@ -6,6 +6,7 @@ import responses from "../utils/response.js";
 import Post from "../models/post.js";
 import codes from "../utils/codes.js";
 import PostHandler from "../controllers/post.js";
+import { REQ_TYPE } from "../utils/request.js";
 
 const router = Router();
 
@@ -13,7 +14,7 @@ const router = Router();
 router.use(authentication.validateAuth);
 
 router.get("/", async (req, res) => {
-  const handler = new PostHandler(req.user);
+  const handler = new PostHandler(req.user, req.query);
   if (!checks.isNull(await handler.setup(res))) return;
 
   return await handler.getPosts(res);
@@ -27,162 +28,24 @@ router.post("/", async (req, res) => {
 });
 
 router.patch("/", async (req, res) => {
-  let { statusCode, message, userId } = await userDb.getUserIdFromUsername(
-    req.user.username
-  );
+  const handler = new PostHandler(req.user, req.body);
+  if (!checks.isNull(await handler.setup(res))) return;
 
-  if (checks.isNull(userId)) return responses.serve(res, statusCode, message);
-
-  // select what needs to patched
-  const postTitle = req.body.title ?? null,
-    postContent = req.body.content ?? null,
-    postImage = req.body.image ?? null,
-    postId = req.body.id ?? null;
-
-  if (checks.isNull(postId)) {
-    statusCode = codes.clientError.BAD_REQUEST;
-    message = "Post Id missing in the request";
-    return responses.serve(res, statusCode, message);
-  }
-
-  const updatingData = {
-    title: postTitle,
-    content: postContent,
-    image: postImage,
-  };
-
-  const toBeUpdated = {};
-
-  for (const uData in updatingData) {
-    if (!checks.isNull(updatingData[uData]))
-      toBeUpdated[uData] = updatingData[uData];
-  }
-
-  // update the post with the data that needs to be updated
-  let post;
-  try {
-    post = await Post.update(toBeUpdated, {
-      where: {
-        id: postId,
-        userId: userId,
-      },
-    });
-  } catch (err) {
-    console.log(err);
-
-    statusCode = codes.serverError.INTERNAL_SERVER_ERROR;
-    message = "couldn't handle the request right now, try again later";
-    return responses.serve(res, statusCode, message);
-  }
-
-  // if update occured, send the OK and if not NOT_FOUND
-  if (post[0] === 0) {
-    statusCode = codes.patch.NOT_FOUND;
-    message = "No post with the credentials given found";
-  } else {
-    statusCode = codes.patch.OK;
-    message = "Post updated";
-  }
-
-  responses.serve(res, statusCode, message);
+  return handler.updatePost(res, REQ_TYPE.PATCH);
 });
 
 router.put("/", async (req, res) => {
-  let { statusCode, message, userId } = await userDb.getUserIdFromUsername(
-    req.user.username
-  );
+  const handler = new PostHandler(req.user, req.body);
+  if (!checks.isNull(await handler.setup(res))) return;
 
-  if (checks.isNull(userId)) return responses.serve(res, statusCode, message);
-
-  const postTitle = req.body.title ?? null,
-    postContent = req.body.content ?? null,
-    postImage = req.body.image ?? null,
-    postId = req.body.id ?? null;
-
-  if (checks.isNull(postTitle)) {
-    statusCode = codes.clientError.BAD_REQUEST;
-    message = "Post title cannot be set to null.";
-    return responses.serve(res, statusCode, message);
-  }
-  if (checks.isNull(postId)) {
-    statusCode = codes.clientError.BAD_REQUEST;
-    message = "Post Id missing in the request";
-    return responses.serve(res, statusCode, message);
-  }
-
-  const updatingData = {
-    title: postTitle,
-    content: postContent,
-    image: postImage,
-  };
-
-  let updated;
-  try {
-    updated = await Post.update(updatingData, {
-      where: {
-        id: postId,
-        userId: userId,
-      },
-    });
-  } catch (err) {
-    console.log(err);
-
-    statusCode = codes.serverError.INTERNAL_SERVER_ERROR;
-    message = "couldn't handle the request, try again later";
-    return responses.serve(res, statusCode, message);
-  }
-
-  if (updated[0] === 0) {
-    statusCode = codes.patch.NOT_FOUND;
-    message = "No post with the credentials given found";
-  } else {
-    statusCode = codes.patch.OK;
-    message = "Post updated";
-  }
-
-  responses.serve(res, statusCode, message);
+  return handler.updatePost(res, REQ_TYPE.PUT);
 });
 
 router.delete("/", async (req, res) => {
-  let { statusCode, message, userId } = await userDb.getUserIdFromUsername(
-    req.user.username
-  );
+  const handler = new PostHandler(req.user, req.query);
+  if (!checks.isNull(await handler.setup(res))) return;
 
-  if (checks.isNull(userId)) return responses.serve(res, statusCode, message);
-
-  const postId = req.query.id ?? null;
-
-  if (checks.isNull(postId)) {
-    statusCode = codes.clientError.BAD_REQUEST;
-    message = "Post Id missing in the request";
-    return responses.serve(res, statusCode, message);
-  }
-
-  let deleted;
-  try {
-    deleted = await Post.destroy({
-      where: {
-        id: postId,
-        userId: userId,
-      },
-    });
-  } catch (err) {
-    console.log(err);
-
-    statusCode = codes.serverError.INTERNAL_SERVER_ERROR;
-    message = "couldn't handle the request, try again later";
-    return responses.serve(res, statusCode, message);
-  }
-
-  if (deleted === 0) {
-    statusCode = codes.clientError.BAD_REQUEST;
-    message = "no post with credentials given found";
-  } else {
-    statusCode = codes.delete.NO_CONTENT;
-    message = "post deleted successfully";
-  }
-
-  responses.serve(res, statusCode, message);
+  return handler.deletePost(res);
 });
 
 export const PostRouter = router;
